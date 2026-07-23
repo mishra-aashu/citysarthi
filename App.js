97630cf677dbcbc2010c2d2b9b495d47b81e2c55
Aashu <aashutoshk625@gmail.com>
2026-07-24 00:22:52 +0530

    feat: add adaptive icon, favicon, and app icon assets

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { AuthProvider } from './src/context/AuthContext';
 import { LocationProvider } from './src/context/LocationContext';
 
 export default function App() {
+  useEffect(() => {
+    if (Platform.OS === 'web' && typeof document !== 'undefined') {
+      document.title = "CitySarthi - Smart Mobility & Vehicle Rental";
+      let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+      link.type = 'image/png';
+      link.rel = 'shortcut icon';
+      link.href = '/src/assets/images/favicon.png';
+      document.getElementsByTagName('head')[0].appendChild(link);
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <LocationProvider>
